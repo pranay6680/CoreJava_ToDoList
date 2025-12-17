56ccd8ea6329cfa5865bb8d9dc63bfe1e55294c3
pranay6680 <pranay6680@gmail.com>
2025-12-17 19:03:59 +0530

    code split

--- src/ToDoListMain.java
+++ src/ToDoListMain.java
@@ -3,13 +3,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 
-public class ToDoList2 {
+public class ToDoListMain {
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         HashMap<Integer, String> hs = new HashMap<>();
-        Practice pp = new Practice();
+        PracticeService pp = new PracticeService();
         pp.m1(br,hs);
     }
         }
